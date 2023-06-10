19ae855521caab6a04f6048725c3d729eef057e9
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-10 12:51:27 -0400

    Creé el header y añadi la interacción al input del dark-button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        "clamp-md": "clamp(20px, 6vw, 26px)",
+        "clamp-sm": "clamp(18px, 5vw, 20px)",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
