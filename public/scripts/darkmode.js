c12f2ec654cebacc4237d202b95e192c62ce4e77
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-12 12:29:51 -0400

    Arreglé el botón de darkmode e hice responsive el Hero y empcé con el diseño de las opiniones

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -2,25 +2,24 @@ const d = document;
 const w = window;
 
 w.addEventListener("DOMContentLoaded", () => {
-  // TODO: Cambiar el nombre de las variables por $elemento
-  const switchButton = d.getElementById("switch-button");
-  const roundButton = d.getElementById("round-button");
-  const headerLogo = d.getElementById("header-logo");
-  const heroImg = d.getElementById("hero-img");
+  const $roundButton = d.getElementById("round-button");
+  const $switchButton = d.getElementById("switch-button");
+  const $headerLogo = d.getElementById("header-logo");
+  const $heroImg = d.getElementById("hero-img");
 
   d.getElementById("switch-label").addEventListener("click", () => {
-    const isDarkMode = roundButton.classList.toggle("translate-x-full");
+    const isDarkMode = $roundButton.classList.toggle("translate-x-full");
 
     const mode = isDarkMode ? "dark" : "light";
     const logo = `/assets/logos/${mode}-logo.png`;
-    const heroImgMode = `/assets/hero-${mode}-img.png`;
     const roundButtonImage = `/assets/switch-mode/round-${mode}-button.png`;
     const switchButtonImage = `/assets/switch-mode/switch-${mode}-button.png`;
+    const heroImgMode = `/assets/hero-${mode}-img.png`;
 
-    heroImg.src = heroImgMode;
-    roundButton.src = roundButtonImage;
-    switchButton.src = switchButtonImage;
-    headerLogo.src = logo;
+    $heroImg.src = heroImgMode;
+    $roundButton.src = roundButtonImage;
+    $switchButton.src = switchButtonImage;
+    $headerLogo.src = logo;
 
     // Data Atributos
     const darkmodeElements = d.querySelectorAll("[data-darkmode]");
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,17 @@ module.exports = {
   theme: {
     extend: {
       width: {
+        "logo-header-clamp": "clamp(200px, 80vw, 300px)",
         "hero-clamp": "clamp(340px, 40vw, 500px)",
       },
+      fontFamily: {
+        montserrat: "font-family: 'Montserrat', sans-serif",
+        "sans-serif": "font-family: 'Open Sans', sans-serif",
+      },
       fontSize: {
-        "clamp-md": "clamp(20px, 6vw, 26px)",
         "clamp-sm": "clamp(18px, 5vw, 20px)",
+        "clamp-md": "clamp(20px, 6vw, 26px)",
+        "clamp-lg": "clamp(24px, 8vw, 32px)",
       },
       dropShadow: {
         custom: "1px 1px 2px #000",
@@ -19,10 +25,18 @@ module.exports = {
         "just-white": "#fff",
         "main-button": "#F13030",
         // Light Mode
-        "light-blue": "#479EF9",
+        "blue-light": "#479EF9",
         // Dark Mode
-        "dark-blue": "#003369",
+        "blue-dark": "#003369",
       },
+      backgroundImage: (theme) => ({
+        "gradient-radial": `radial-gradient(${theme(
+          "colors.blue.500"
+        )} 0%, ${theme("colors.blue.700")} 100%)`,
+        "gradient-linear": `linear-gradient(0deg, ${theme(
+          "colors.blue.500"
+        )} 0%, ${theme("colors.blue.700")} 100%)`,
+      }),
     },
   },
   variants: {},
