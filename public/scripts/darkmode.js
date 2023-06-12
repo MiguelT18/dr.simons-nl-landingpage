ff010cdc1b41206c285c7e8af0d7e19bd7668d33
Miguel-laptop <miguel.teranj02@gmail.com>
2023-06-12 00:07:48 -0400

    Añadí más funcionalidad al botón del darkmode y terminé el Hero y ya está responsive

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -2,28 +2,38 @@ const d = document;
 const w = window;
 
 w.addEventListener("DOMContentLoaded", () => {
+  // TODO: Cambiar el nombre de las variables por $elemento
   const switchButton = d.getElementById("switch-button");
   const roundButton = d.getElementById("round-button");
   const headerLogo = d.getElementById("header-logo");
+  const heroImg = d.getElementById("hero-img");
 
   d.getElementById("switch-label").addEventListener("click", () => {
-    roundButton.classList.toggle("translate-x-full");
-    if (roundButton.classList.contains("translate-x-full")) {
-      roundButton.src = "/assets/switch-mode/round-light-button.png";
-      switchButton.src = "/assets/switch-mode/switch-light-button.png";
-      // Cambiar header-logo a light
-      headerLogo.src = "/assets/logos/light-logo.png";
+    const isDarkMode = roundButton.classList.toggle("translate-x-full");
 
-      // Cambiar clase del body a is-active
-      d.body.classList.add("is-active");
+    const mode = isDarkMode ? "dark" : "light";
+    const logo = `/assets/logos/${mode}-logo.png`;
+    const heroImgMode = `/assets/hero-${mode}-img.png`;
+    const roundButtonImage = `/assets/switch-mode/round-${mode}-button.png`;
+    const switchButtonImage = `/assets/switch-mode/switch-${mode}-button.png`;
+
+    heroImg.src = heroImgMode;
+    roundButton.src = roundButtonImage;
+    switchButton.src = switchButtonImage;
+    headerLogo.src = logo;
+
+    // Data Atributos
+    const darkmodeElements = d.querySelectorAll("[data-darkmode]");
+
+    // Aplica la clase a todos los elementos seleccionados
+    darkmodeElements.forEach((e) => {
+      e.classList.toggle("is-active", isDarkMode);
+    });
+
+    if (isDarkMode) {
+      d.documentElement.classList.add("dark");
     } else {
-      roundButton.src = "/assets/switch-mode/round-dark-button.png";
-      switchButton.src = "/assets/switch-mode/switch-dark-button.png";
-      // Cambiar header-logo a dark
-      headerLogo.src = "/assets/logos/dark-logo.jpg";
-
-      // Quitar clase is-active a body
-      d.body.classList.remove("is-active");
+      d.documentElement.classList.remove("dark");
     }
   });
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
+      width: {
+        "hero-clamp": "clamp(340px, 40vw, 500px)",
+      },
       fontSize: {
         "clamp-md": "clamp(20px, 6vw, 26px)",
         "clamp-sm": "clamp(18px, 5vw, 20px)",
+      },
+      dropShadow: {
+        custom: "1px 1px 2px #000",
+      },
+      colors: {
+        "just-black": "#000",
+        "just-white": "#fff",
+        "main-button": "#F13030",
+        // Light Mode
+        "light-blue": "#479EF9",
+        // Dark Mode
+        "dark-blue": "#003369",
       },
     },
   },
