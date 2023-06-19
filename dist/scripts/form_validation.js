d267bfe58c9988f3912f2881248d63c6c661db3f
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-19 18:42:19 -0400

    intengrando la página de gracias después del registro de la consulta...

--- dist/scripts/form_validation.js
+++ dist/scripts/form_validation.js
@@ -24,6 +24,7 @@ const patterns = {
 
 // Handle input validation
 inputs.name.addEventListener("keyup", (e) => {
+  //? input name
   let inputValue = e.target.value;
   if (!patterns.name.test(inputValue)) {
     inputValue = inputValue.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ´\s]/g, "");
@@ -36,6 +37,7 @@ inputs.name.addEventListener("keyup", (e) => {
 });
 
 inputs.email.addEventListener("keyup", (e) => {
+  //? input email
   if (inputs.email.value !== "") {
     errors.email.classList.remove("block");
     errors.email.classList.add("hidden");
@@ -45,11 +47,42 @@ inputs.email.addEventListener("keyup", (e) => {
 $form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
+  let isValid = true;
+
   Object.keys(inputs).forEach((key) => {
     if (inputs[key].value === "") {
       errors[key].textContent = "Este campo no puede ir vacio";
       errors[key].classList.add("block");
       errors[key].classList.remove("hidden");
+      isValid = false;
+      return;
     }
   });
+
+  if (isValid) {
+    // Realiza la solicitud HTTP para enviar los datos del formulario al servidor
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/contacts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: inputs.name.value,
+          email: inputs.email.value,
+        }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        // La solicitud ha sido exitosa (código de respuesta 200-299)
+        console.log("Solicitud Exitosa");
+      } else {
+        // La solicitud no fue exitosa (código de respuesta fuera del rango 200-299)
+        console.error("Error en la solicitud: ", data);
+      }
+    } catch (error) {
+      console.error("Hubo un error durante la solicitud: ", error);
+    }
+  }
 });
--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -7,49 +7,52 @@ w.addEventListener("DOMContentLoaded", () => {
   const $headerLogo = d.getElementById("header-logo");
   const $heroImg = d.getElementById("hero-img");
 
-  // Recupera el modo oscuro guardado del localStorage
-  const savedDarkMode = localStorage.getItem("darkMode") === "true";
+  // Verifica si los elementos existen antes de intentar interactuar con ellos
+  if ($roundButton && $switchButton && $headerLogo && $heroImg) {
+    // Recupera el modo oscuro guardado del localStorage
+    const savedDarkMode = localStorage.getItem("darkMode") === "true";
 
-  // Aplica el modo oscuro si estaba guardado en localStorage
-  if (savedDarkMode) {
-    toggleDarkMode(true);
-    $roundButton.classList.add("translate-x-full");
-  }
+    // Aplica el modo oscuro si estaba guardado en localStorage
+    if (savedDarkMode) {
+      toggleDarkMode(true);
+      $roundButton.classList.add("translate-x-full");
+    }
 
-  d.getElementById("switch-label").addEventListener("click", () => {
-    // Comprueba si el modo oscuro está activado
-    const isDarkMode = $roundButton.classList.toggle("translate-x-full");
+    d.getElementById("switch-label").addEventListener("click", () => {
+      // Comprueba si el modo oscuro está activado
+      const isDarkMode = $roundButton.classList.toggle("translate-x-full");
 
-    // Guarda el estado del modo oscuro en localStorage
-    localStorage.setItem("darkMode", isDarkMode);
+      // Guarda el estado del modo oscuro en localStorage
+      localStorage.setItem("darkMode", isDarkMode);
 
-    toggleDarkMode(isDarkMode);
-  });
-
-  function toggleDarkMode(isDarkMode) {
-    const mode = isDarkMode ? "dark" : "light";
-    const logo = `/assets/logos/${mode}-logo.png`;
-    const roundButtonImage = `/assets/switch-mode/round-${mode}-button.png`;
-    const switchButtonImage = `/assets/switch-mode/switch-${mode}-button.png`;
-    const heroImgMode = `/assets/hero-${mode}-img.png`;
-
-    $heroImg.src = heroImgMode;
-    $roundButton.src = roundButtonImage;
-    $switchButton.src = switchButtonImage;
-    $headerLogo.src = logo;
-
-    // Data Atributos
-    const darkmodeElements = d.querySelectorAll("[data-darkmode]");
-
-    // Aplica la clase a todos los elementos seleccionados
-    darkmodeElements.forEach((e) => {
-      e.classList.toggle("is-active", isDarkMode);
+      toggleDarkMode(isDarkMode);
     });
 
-    if (isDarkMode) {
-      d.documentElement.classList.add("dark");
-    } else {
-      d.documentElement.classList.remove("dark");
+    function toggleDarkMode(isDarkMode) {
+      const mode = isDarkMode ? "dark" : "light";
+      const logo = `/assets/logos/${mode}-logo.png`;
+      const roundButtonImage = `/assets/switch-mode/round-${mode}-button.png`;
+      const switchButtonImage = `/assets/switch-mode/switch-${mode}-button.png`;
+      const heroImgMode = `/assets/hero-${mode}-img.png`;
+
+      $heroImg.src = heroImgMode;
+      $roundButton.src = roundButtonImage;
+      $switchButton.src = switchButtonImage;
+      $headerLogo.src = logo;
+
+      // Data Atributos
+      const darkmodeElements = d.querySelectorAll("[data-darkmode]");
+
+      // Aplica la clase a todos los elementos seleccionados
+      darkmodeElements.forEach((e) => {
+        e.classList.toggle("is-active", isDarkMode);
+      });
+
+      if (isDarkMode) {
+        d.documentElement.classList.add("dark");
+      } else {
+        d.documentElement.classList.remove("dark");
+      }
     }
   }
 });
