b11738d7b50fd6ff93a006cc59afa91fe741992f
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-21 12:47:59 -0400

    botón modo oscuro terminado y optimizado para ser reutilizabe (guardar sesión del usuario en el localStorage)

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -1,4 +1,34 @@
 const d = document;
 const w = window;
 
-w.addEventListener("DOMContentLoaded", () => {});
+w.addEventListener("DOMContentLoaded", () => {
+  d.getElementById("switch").addEventListener("click", (e) => {
+    const isChecked = e.target.checked;
+
+    // elements
+    const $switchLightButton = d.getElementById("switch-light-button"),
+      $roundLightButton = d.getElementById("round-light-button");
+
+    d.body.classList.toggle("is-active", isChecked);
+
+    // data attributes
+    const $dataAttributes = d.querySelectorAll("[data-darkmode]");
+    $dataAttributes.forEach((e) => {
+      e.classList.toggle("dark");
+    });
+
+    if (isChecked) {
+      $switchLightButton.src = "/assets/switch-mode/switch-dark-button.png";
+      $roundLightButton.src = "/assets/switch-mode/round-dark-button.png";
+
+      // round button translation
+      $roundLightButton.classList.add("translate-x-full");
+    } else {
+      $switchLightButton.src = "/assets/switch-mode/switch-light-button.png";
+      $roundLightButton.src = "/assets/switch-mode/round-light-button.png";
+
+      // round button translation
+      $roundLightButton.classList.remove("translate-x-full");
+    }
+  });
+});
