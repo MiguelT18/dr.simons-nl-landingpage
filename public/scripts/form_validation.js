1dd39cb727dd79d69808aeac03f629500ca89170
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-26 20:10:16 -0400

    cambié el botón del formulario por una etiqueta de anclaje

--- public/scripts/form_validation.js
+++ public/scripts/form_validation.js
@@ -1,88 +1,64 @@
-import { API_TOKEN, LIST_ID } from "./config.js";
-
 const d = document;
 
-// Form element
 const $form = d.getElementById("form");
 
-// Input elements
 const inputs = {
   name: d.getElementById("input-name"),
   email: d.getElementById("input-email"),
 };
 
-// Error elements
 const errors = {
   name: d.getElementById("error-name"),
   email: d.getElementById("error-email"),
 };
 
-// Regex patterns
-const patterns = {
-  name: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ´\s]*$/,
-};
+const $button = d.getElementById("submit-button");
 
-// Handle input validation
-inputs.name.addEventListener("keyup", (e) => {
-  //? input name
+// Validar el envío del formulario
+$form.addEventListener("submit", (e) => {
+  //? form.submit
+  // Validar el envío del campo del nombre
+  if (inputs.name.value === "") {
+    errors.name.innerText = "Este campo no puede ir vacio";
+    errors.name.classList.remove("hidden");
+  } else {
+    errors.name.innerText = "";
+  }
+
+  // Validar el envío del campo del email
+  if (inputs.email.value === "") {
+    errors.email.innerText = "Este campo no puede ir vacio";
+    errors.email.classList.remove("hidden");
+  } else {
+    errors.email.innerText = "";
+  }
+});
+
+// Validar el campo del nombre
+inputs.name.addEventListener("input", (e) => {
+  //? input.name
   let inputValue = e.target.value;
-  if (!patterns.name.test(inputValue)) {
-    inputValue = inputValue.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ´\s]/g, "");
-    inputs.name.value = inputValue;
-  }
-  if (inputs.name.value !== "") {
-    errors.name.classList.remove("block");
-    errors.name.classList.add("hidden");
-  }
-});
+  const reg = /[^a-zA-Z\s´áéíóúÁÉÍÓÚ]/g;
+  // Reemplazar los caracteres inválidos por una cadena vacía
+  e.target.value = inputValue.replace(reg, "");
 
-inputs.email.addEventListener("keyup", (e) => {
-  //? input email
-  if (inputs.email.value !== "") {
-    errors.email.classList.remove("block");
-    errors.email.classList.add("hidden");
+  if (inputValue === "") {
+    errors.name.classList.remove("hidden");
+    errors.name.innerHTML = "Este campo no puede ir vacío";
+  } else {
+    errors.name.innerText = "";
   }
 });
 
-$form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+// Validar el campo del correo electrónico
+inputs.email.addEventListener("input", (e) => {
+  //? input.email
+  let inputValue = e.target.value;
+  const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  let isValid = true;
-
-  Object.keys(inputs).forEach((key) => {
-    if (inputs[key].value === "") {
-      errors[key].textContent = "Este campo no puede ir vacio";
-      errors[key].classList.add("block");
-      errors[key].classList.remove("hidden");
-      isValid = false;
-      return;
-    }
-  });
-
-  if (isValid) {
-    // Realiza la solicitud HTTP para enviar los datos del formulario al servidor
-    try {
-      const response = await fetch("http://127.0.0.1:5000/api/contacts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: inputs.name.value,
-          email: inputs.email.value,
-        }),
-      });
-
-      const data = await response.json();
-      if (!response.ok) {
-        // La solicitud ha sido exitosa (código de respuesta 200-299)
-        console.log("Solicitud Exitosa");
-      } else {
-        // La solicitud no fue exitosa (código de respuesta fuera del rango 200-299)
-        console.error("Error en la solicitud: ", data);
-      }
-    } catch (error) {
-      console.error("Hubo un error durante la solicitud: ", error);
-    }
+  if (!inputValue === "") {
+    errors.name.classList.add("remove");
   }
 });
+
+// TODO: Conectar el botón con las validaciones del formulario
