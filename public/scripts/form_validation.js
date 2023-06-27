95ae26f46551065a0b64ea5c6deb3ee04e5f5cd8
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-27 11:47:12 -0400

    validación del formulario de parte del cliente

--- public/scripts/form_validation.js
+++ public/scripts/form_validation.js
@@ -1,64 +1,76 @@
+/** @format */
+
+// Global objects
 const d = document;
+const w = window;
 
 const $form = d.getElementById("form");
 
 const inputs = {
-  name: d.getElementById("input-name"),
-  email: d.getElementById("input-email"),
+	name: d.getElementById("input-name"),
+	email: d.getElementById("input-email"),
 };
 
 const errors = {
-  name: d.getElementById("error-name"),
-  email: d.getElementById("error-email"),
+	name: d.getElementById("error-name"),
+	email: d.getElementById("error-email"),
 };
 
 const $button = d.getElementById("submit-button");
 
 // Validar el envío del formulario
+//? form.submit
 $form.addEventListener("submit", (e) => {
-  //? form.submit
-  // Validar el envío del campo del nombre
-  if (inputs.name.value === "") {
-    errors.name.innerText = "Este campo no puede ir vacio";
-    errors.name.classList.remove("hidden");
-  } else {
-    errors.name.innerText = "";
-  }
+	e.preventDefault();
 
-  // Validar el envío del campo del email
-  if (inputs.email.value === "") {
-    errors.email.innerText = "Este campo no puede ir vacio";
-    errors.email.classList.remove("hidden");
-  } else {
-    errors.email.innerText = "";
-  }
+	// Validar el envío del campo del nombre
+	if (inputs.name.value === "") {
+		errors.name.innerText = "Este campo no puede ir vacio";
+		errors.name.classList.remove("hidden");
+	} else {
+		errors.name.innerText = "";
+	}
+
+	// Validar el envío del campo del email
+	if (inputs.email.value === "") {
+		errors.email.innerText = "Este campo no puede ir vacio";
+		errors.email.classList.remove("hidden");
+	} else {
+		errors.email.innerText = "";
+	}
+
+	// Verificar si no hay errores de validación
+	if (inputs.name.value !== "" && inputs.email.value !== "") {
+		// Redirigir al usuario a la página de agradecimiento
+		w.location.href = "/thanks";
+	}
 });
 
 // Validar el campo del nombre
+//? input.name
 inputs.name.addEventListener("input", (e) => {
-  //? input.name
-  let inputValue = e.target.value;
-  const reg = /[^a-zA-Z\s´áéíóúÁÉÍÓÚ]/g;
-  // Reemplazar los caracteres inválidos por una cadena vacía
-  e.target.value = inputValue.replace(reg, "");
+	let inputValue = e.target.value;
+	const reg = /[^a-zA-Z\s´áéíóúÁÉÍÓÚ]/g;
+	// Reemplazar los caracteres inválidos por una cadena vacía
+	e.target.value = inputValue.replace(reg, "");
 
-  if (inputValue === "") {
-    errors.name.classList.remove("hidden");
-    errors.name.innerHTML = "Este campo no puede ir vacío";
-  } else {
-    errors.name.innerText = "";
-  }
+	if (inputValue === "") {
+		errors.name.classList.remove("hidden");
+		errors.name.innerHTML = "Este campo no puede ir vacío";
+	} else {
+		errors.name.innerText = "";
+	}
 });
 
 // Validar el campo del correo electrónico
 inputs.email.addEventListener("input", (e) => {
-  //? input.email
-  let inputValue = e.target.value;
-  const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	//? input.email
+	let inputValue = e.target.value;
+	const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  if (!inputValue === "") {
-    errors.name.classList.add("remove");
-  }
+	if (!inputValue === "") {
+		errors.name.classList.add("remove");
+	}
 });
 
 // TODO: Conectar el botón con las validaciones del formulario
